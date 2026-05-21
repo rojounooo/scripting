f00fb93ba40d28689f18d86abcb26e8ec46d6d6b
rojounooo <rojounooo@gmail.com>
2026-05-21 14:11:00 +0100

    created functions

--- 00-fundamentals/scripts/bash/functions.sh
+++ 00-fundamentals/scripts/bash/functions.sh
@@ -2,7 +2,17 @@
 
 # Define the function 
 say_hello() {
+    echo "Hello" 
+}
 
+# Define a function with parameters
+# Local keeps the variable within the function only rather than a global function
+
+greet() {
+    local name=$1
+    echo "Hello, $name!"
 }
 
 say_hello
+
+greet "Radwan"
