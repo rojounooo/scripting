093c486ea1f8259f4740a892bc6b1c09d9b9ff62
rojounooo <rojounooo@gmail.com>
2026-05-22 21:34:26 +0100

    ad user creation script

--- 01-users/scripts/powershell/create_ad_user.ps1
+++ 01-users/scripts/powershell/create_ad_user.ps1
@@ -1,11 +1,32 @@
 param(
-    [string]$Username
+    [string]$name,
+    [string]$sam,
+    [string]$givenName,
+    [string]$familyName
 )
 
-if (-not $Username) {
-    Write-Host "Error: No username provided. Usage: .\query_ad_users .ps1 -Username <username>"
+if (-not $name) {
+    Write-Host "Error: No name provided. Usage: .\add_ad_user.ps1 -name <name>"
     exit 1
 }
 
 $server = "192.168.1.100"
 $cred   = Get-Credential "LAB\Administrator"
+
+try {
+    New-ADUser -Name $name `
+               -SamAccountName $sam `
+               -UserPrincipalName "$givenName.$familyName@lab.local" `
+               -GivenName $givenName `
+               -Surname $familyName `
+               -AccountPassword (ConvertTo-SecureString "Password123!" -AsPlainText -Force) `
+               -Enabled $true `
+               -Server $server `
+               -Credential $cred `
+               -ErrorAction Stop
+
+    Write-Host "User '$name' created successfully"
+} catch {
+    Write-Host "Error: $_"
+    exit 1
+}
