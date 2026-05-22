5f80bd773e88d78b883e04d243f77eef8ac400ce
rojounooo <rojounooo@gmail.com>
2026-05-22 18:01:07 +0100

    create user script in bash

---
+++ 01-users/scripts/bash/create_user.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+# Exit on error
+set -e
+
+# Check username arg 
+if [ -z "$1" ]; then    
+    echo "Username argument missing" 
+    exit 1 
+fi 
+
+# Parse username and password
+username=$1 
+password=$(openssl rand -base64 12)
+
+# Create the user with a home directory 
+sudo useradd -m -s /bin/bash "$username"
+
+# Set temporary password 
+echo "$username:$password" | sudo chpasswd
+
+sudo chage -d 0 "$username""
+
+echo "$username has been created"
+echo "Temporary password: $password" 
+echo "user will be prompted to change password at first login" 
+
